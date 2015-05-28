f243e3252403df09c0e163a647ce51c660db4cda
Katelyn <godfrey.katelyn@gmail.com>
2015-05-28 13:28:43 -0400

    Initial commit pt 2

---
+++ Assets/Scripts/QuitApp.cs
@@ -0,0 +1,30 @@
+﻿/*------------------------------------------------------
+Project: AR_test
+Program: QuitApp.cs
+Author: Katelyn Godfrey
+
+Description: Close application when X button is pressed.
+
+Updated: 05/28/2015 
+------------------------------------------------------
+
+ADDED TO ARCAMERA GAME OBJECT.
+
+ON QUIT_BTN ON CLICK TELL IT TO TRIGGER QUITAPP.EXIT.
+
+------------------------------------------------------*/
+
+using UnityEngine;
+using System.Collections;
+
+public class QuitApp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void Exit() {
+		Application.Quit(); 
+	}
+}
